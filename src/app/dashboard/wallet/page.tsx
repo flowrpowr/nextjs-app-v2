3dc7d87f2546931becdf9b2b17dfed86433d1e93
CousCous08 <marcusnglee@gmail.com>
2025-03-10 11:12:20 +0800

    QOL fixes

--- src/app/dashboard/wallet/page.tsx
+++ src/app/dashboard/wallet/page.tsx
@@ -5,34 +5,42 @@ import { useEnokiFlow } from "@mysten/enoki/react";
 import { Button } from "@/components/ui/button";
 
 export default function Wallet() {
-    const enokiFlow = useEnokiFlow();
+  const enokiFlow = useEnokiFlow();
 
-    return (
+  return (
+    <>
+      <div className="items-center">
+        <h3>
+          Before listening, click here to create a Sui wallet! Use the same
+          google sign-in
+        </h3>
         <Button onClick={handleSignIn} variant="outline">
-            Button
+          Create Wallet!
         </Button>
-    );
+      </div>
+    </>
+  );
 
-    function handleSignIn() {
-        const protocol = window.location.protocol;
-        const host = window.location.host;
-        const redirectUrl = `${protocol}//${host}/dashboard/wallet/enoki`;
+  function handleSignIn() {
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    const redirectUrl = `${protocol}//${host}/dashboard/wallet/enoki`;
 
-        enokiFlow
-            .createAuthorizationURL({
-                provider: "google",
-                network: "testnet",
-                clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-                redirectUrl,
-                extraParams: {
-                    scope: ["openid", "email", "profile"],
-                },
-            })
-            .then((url) => {
-                window.location.href = url;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+    enokiFlow
+      .createAuthorizationURL({
+        provider: "google",
+        network: "testnet",
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+        redirectUrl,
+        extraParams: {
+          scope: ["openid", "email", "profile"],
+        },
+      })
+      .then((url) => {
+        window.location.href = url;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
